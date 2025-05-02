1197110e2e11fb19cc905809226751f9ad1ee4b6
nasif07 <nasifulislamnasif23@gmail.com>
2025-05-02 15:58:57 +0600

    resolved animations

--- src/Section/ContactMe/ContactMe.jsx
+++ src/Section/ContactMe/ContactMe.jsx
@@ -4,81 +4,127 @@ import Container from "../../components/Container";
 import Title from "../../components/Title";
 
 const ContactMe = () => {
-    return (
-        <section id="contact" className="bg-[#060606] text-white">
-            <Title first={"Contact"} last={"Me"}></Title>
-            <div className="hero min-h-screen ">
-
-                <Container>
-                    <div className="lg:flex gap-4">
-                        <div   data-aos="" data-aos-duration="1000" className="flex-1 space-y-3">
-                            <h5 className="text-[25px] text-[#B1B1A2]">Get In Touch</h5>
-                            <h1 className="md:text-6xl text-4xl font-bold">Let’s Talk For your <br /> <span className="text-[#C9F31D]">Next Projects</span></h1>
-                            <p className="py-6 text-[#B1B1A2]">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                            <p className="text-[29px] font-semibold">More Info</p>
-                            <div className="space-y-5">
-                                <div className="flex gap-6 items-center">
-                                    <FaLocationArrow className="text-[#C9F31D] text-3xl"></FaLocationArrow>
-                                    <p className="text-[#B1B1A2] text-xl">Chattogram, Bangladesh</p>
-                                </div>
-                                <div className="flex gap-6 items-center">
-                                    <MdOutlineEmail className="text-[#C9F31D] text-3xl"></MdOutlineEmail>
-                                    <p className="text-[#B1B1A2] md:text-xl">nasifulislamnasif23@gmail.com</p>
-                                </div>
-                                <div className="flex gap-6 pb-6 items-center md:pb-6">
-                                    <FaPhone className="text-[#C9F31D] text-3xl"></FaPhone>
-                                    <p className="text-[#B1B1A2] text-xl">01849206410</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div data-aos="fade-up" data-aos-duration="1000" className="bg-[#1F1F1F] p-8 rounded-3xl flex-1 mb-12">
-                            <div className="lg:flex gap-6 pb-6">
-                                <label className="form-control w-full">
-                                    <div className="label">
-                                        <span className="label-text text-xl pb-3 text-white">What is your name? <span className="text-[25px] text-[#C9F31D]">*</span></span>
-                                    </div>
-                                    <input type="text" placeholder="Write Your Full Name" className="input rounded-xl input-bordered  w-full  md:input-lg bg-[#2F2F2F]" />
-                                </label>
-                                <label className="form-control w-full">
-                                    <div className="label">
-                                        <span className="label-text text-xl pb-3 text-white">Email Address  <span className="text-[25px] text-[#C9F31D]">*</span></span>
-                                    </div>
-                                    <input type="text" placeholder="Type Your Email" className="input rounded-xl input-bordered w-full  md:input-lg bg-[#2F2F2F]" />
-                                </label>
-                            </div>
-                            <div className="lg:flex gap-6  pb-6">
-                                <label className="form-control w-full">
-                                    <div className="label">
-                                        <span className="label-text text-xl pb-3 text-white">Phone Number <span className="text-[25px] text-[#C9F31D]">*</span></span>
-                                    </div>
-                                    <input type="text" placeholder="Type Your Number" className="input rounded-xl input-bordered w-full  md:input-lg bg-[#2F2F2F]" />
-                                </label>
-                                <label className="form-control w-full">
-                                    <div className="label">
-                                        <span className="label-text text-xl pb-3 text-white">Subject <span className="text-[25px] text-[#C9F31D]">*</span></span>
-                                    </div>
-                                    <input type="text" placeholder="I would like to discussed" className="input rounded-xl input-bordered w-full  md:input-lg bg-[#2F2F2F]" />
-                                </label>
-                            </div>
-                            <label className="form-control">
-                                <div className="label">
-                                    <span className="label-text text-xl pb-3 text-white">Textarea  <span className="text-[25px] text-[#C9F31D]">*</span></span>
-                                </div>
-                                <textarea className="textarea textarea-bordered h-32 rounded-xl bg-[#2F2F2F]" placeholder="Write Message..."></textarea>
-                            </label>
-                            <div className="mt-12">
-                                <button className="btn bg-[#C9F31D] border-0 text-black text-[19px] font-medium px-7"> <FaMessage></FaMessage> Send Me Message</button>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
+  return (
+    <section id="contact" className="bg-[#060606] text-white">
+      <Title first={"Contact"} last={"Me"}></Title>
+      <div className="hero min-h-screen ">
+        <Container>
+          <div className="lg:flex gap-4">
+            <div className="flex-1 space-y-3">
+              <h5 className="text-[25px] text-[#B1B1A2]">Get In Touch</h5>
+              <h1 className="md:text-6xl text-4xl font-bold">
+                Let’s Talk For your <br />{" "}
+                <span className="text-[#C9F31D]">Next Projects</span>
+              </h1>
+              <p className="py-6 text-[#B1B1A2]">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <p className="text-[29px] font-semibold">More Info</p>
+              <div className="space-y-5">
+                <div className="flex gap-6 items-center">
+                  <FaLocationArrow className="text-[#C9F31D] text-3xl"></FaLocationArrow>
+                  <p className="text-[#B1B1A2] text-xl">
+                    Chattogram, Bangladesh
+                  </p>
+                </div>
+                <div className="flex gap-6 items-center">
+                  <MdOutlineEmail className="text-[#C9F31D] text-3xl"></MdOutlineEmail>
+                  <p className="text-[#B1B1A2] md:text-xl">
+                    nasifulislamnasif23@gmail.com
+                  </p>
+                </div>
+                <div className="flex gap-6 pb-6 items-center md:pb-6">
+                  <FaPhone className="text-[#C9F31D] text-3xl"></FaPhone>
+                  <p className="text-[#B1B1A2] text-xl">01849206410</p>
+                </div>
+              </div>
             </div>
-        </section>
-    );
+            <div className="bg-[#1F1F1F] p-8 rounded-3xl flex-1 mb-12">
+              <div className="lg:flex gap-6 pb-6">
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text text-xl pb-3 text-white">
+                      What is your name?{" "}
+                      <span className="text-[25px] text-[#C9F31D]">*</span>
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Write Your Full Name"
+                    className="input rounded-xl input-bordered  w-full  md:input-lg bg-[#2F2F2F]"
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text text-xl pb-3 text-white">
+                      Email Address{" "}
+                      <span className="text-[25px] text-[#C9F31D]">*</span>
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type Your Email"
+                    className="input rounded-xl input-bordered w-full  md:input-lg bg-[#2F2F2F]"
+                  />
+                </label>
+              </div>
+              <div className="lg:flex gap-6  pb-6">
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text text-xl pb-3 text-white">
+                      Phone Number{" "}
+                      <span className="text-[25px] text-[#C9F31D]">*</span>
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="Type Your Number"
+                    className="input rounded-xl input-bordered w-full  md:input-lg bg-[#2F2F2F]"
+                  />
+                </label>
+                <label className="form-control w-full">
+                  <div className="label">
+                    <span className="label-text text-xl pb-3 text-white">
+                      Subject{" "}
+                      <span className="text-[25px] text-[#C9F31D]">*</span>
+                    </span>
+                  </div>
+                  <input
+                    type="text"
+                    placeholder="I would like to discussed"
+                    className="input rounded-xl input-bordered w-full  md:input-lg bg-[#2F2F2F]"
+                  />
+                </label>
+              </div>
+              <label className="form-control">
+                <div className="label">
+                  <span className="label-text text-xl pb-3 text-white">
+                    Textarea{" "}
+                    <span className="text-[25px] text-[#C9F31D]">*</span>
+                  </span>
+                </div>
+                <textarea
+                  className="textarea textarea-bordered h-32 rounded-xl bg-[#2F2F2F]"
+                  placeholder="Write Message..."></textarea>
+              </label>
+              <div className="mt-12">
+                <button className="btn bg-[#C9F31D] border-0 text-black text-[19px] font-medium px-7">
+                  {" "}
+                  <FaMessage></FaMessage> Send Me Message
+                </button>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </section>
+  );
 };
 
 export default ContactMe;
 
-
-
-{/* */ }
+{
+  /* */
+}
--- src/Section/experienceEducation/ExperienceEducation.jsx
+++ src/Section/experienceEducation/ExperienceEducation.jsx
@@ -3,58 +3,86 @@ import Container from "../../components/Container";
 import Title from "../../components/Title";
 import { MdDeveloperMode } from "react-icons/md";
 const ExperienceEducation = () => {
-    return (
-        <section id="backend" className="bg-[#060606] text-white">
-            <Title first={"My"} last={"Backend"}></Title>
-            <Container>
-                <h1 className="text-center pb-14 text-2xl font-semibold">My Educational Background</h1>
-                <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical ">
-                    <li>
-                        <div className="timeline-middle">
-                            <FaBook className="text-[#C9F31D] text-2xl m-3 mt-0"></FaBook>
-                        </div>
-                        <div   data-aos="fade-up" data-aos-duration="1000" className="timeline-start md:text-end mb-10 space-y-2.5 max-w-[450px] ">
-                            <time className="font-mono italic text-[#B1B1A2]">2020-Present</time>
-                            <div className="text-[20px] font-medium text-[#C9F31D]">Diploma In Engineering</div>
-                            <div className="lg:text-2xl md:text-[18px] font-black">Chattogram Polytechnic Institute</div>
-                            <p className="text-[#B1B1A2]">Now I am studying on Chattogram Polytechnic Institute. I am a student of Electronics Department</p>
-                            
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <FaBook className="text-[#C9F31D] text-2xl m-3"></FaBook>
-                        </div>
-                        <div data-aos="fade-up" data-aos-duration="1000" className="timeline-end mb-10 space-y-2.5 max-w-[450px]">
-                        <time className="font-mono italic text-[#B1B1A2]">2019-2020</time>
-                            <div className="text-[20px] font-medium text-[#C9F31D]">SSC / Science</div>
-                            <div className="lg:text-2xl md:text-[18px] font-black">Halishahar Alhaj Mohabbat Ali City Corp. High School</div>
-                            <p className="text-[#B1B1A2]">I completed my SSC examination, In 2020, At the school of Chattogram which is the renowned school of Chattogram that is Halishahar Alhaj Mohobbat Ali City Corporation High School.</p>
-                        </div>
-                        <hr />
-                    </li>
-                </ul>
-                <h1 className="text-center p-14 text-2xl font-semibold">My Experience</h1>
-                <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical lg:pb-16 md:pb-12 pb-6">
-                    <li>
-                        <div className="timeline-middle">
-                            <MdDeveloperMode className="text-[#C9F31D] text-3xl m-3 mt-0"></MdDeveloperMode>
-                        </div>
-                        <div data-aos="fade-up" data-aos-duration="1000" className="timeline-start md:text-end mb-10 space-y-2.5 max-w-[450px]">
-                            <time className="font-mono italic text-[#B1B1A2]">2021-Present</time>
-                            <div className="text-[20px] font-medium text-[#C9F31D]">Web Development</div>
-                            <div className="lg:text-2xl md:text-[18px] font-black">2 year + self learning Experience</div>
-                            <p className="text-[#B1B1A2]">I completed my full Stack Web Development Course from Programming Hero.</p>
-                            
-                        </div>
-                        <hr />
-                    </li>
-                    </ul>
-            </Container>
-        </section>
-    );
+  return (
+    <section id="backend" className="bg-[#060606] text-white">
+      <Title first={"My"} last={"Backend"}></Title>
+      <Container>
+        <h1 className="text-center pb-14 text-2xl font-semibold">
+          My Educational Background
+        </h1>
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical ">
+          <li>
+            <div className="timeline-middle">
+              <FaBook className="text-[#C9F31D] text-2xl m-3 mt-0"></FaBook>
+            </div>
+            <div className="timeline-start md:text-end mb-10 space-y-2.5 max-w-[450px] ">
+              <time className="font-mono italic text-[#B1B1A2]">
+                2020-Present
+              </time>
+              <div className="text-[20px] font-medium text-[#C9F31D]">
+                Diploma In Engineering
+              </div>
+              <div className="lg:text-2xl md:text-[18px] font-black">
+                Chattogram Polytechnic Institute
+              </div>
+              <p className="text-[#B1B1A2]">
+                Now I am studying on Chattogram Polytechnic Institute. I am a
+                student of Electronics Department
+              </p>
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <FaBook className="text-[#C9F31D] text-2xl m-3"></FaBook>
+            </div>
+            <div className="timeline-end mb-10 space-y-2.5 max-w-[450px]">
+              <time className="font-mono italic text-[#B1B1A2]">2019-2020</time>
+              <div className="text-[20px] font-medium text-[#C9F31D]">
+                SSC / Science
+              </div>
+              <div className="lg:text-2xl md:text-[18px] font-black">
+                Halishahar Alhaj Mohabbat Ali City Corp. High School
+              </div>
+              <p className="text-[#B1B1A2]">
+                I completed my SSC examination, In 2020, At the school of
+                Chattogram which is the renowned school of Chattogram that is
+                Halishahar Alhaj Mohobbat Ali City Corporation High School.
+              </p>
+            </div>
+            <hr />
+          </li>
+        </ul>
+        <h1 className="text-center p-14 text-2xl font-semibold">
+          My Experience
+        </h1>
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical lg:pb-16 md:pb-12 pb-6">
+          <li>
+            <div className="timeline-middle">
+              <MdDeveloperMode className="text-[#C9F31D] text-3xl m-3 mt-0"></MdDeveloperMode>
+            </div>
+            <div className="timeline-start md:text-end mb-10 space-y-2.5 max-w-[450px]">
+              <time className="font-mono italic text-[#B1B1A2]">
+                2021-Present
+              </time>
+              <div className="text-[20px] font-medium text-[#C9F31D]">
+                Web Development
+              </div>
+              <div className="lg:text-2xl md:text-[18px] font-black">
+                2 year + self learning Experience
+              </div>
+              <p className="text-[#B1B1A2]">
+                I completed my full Stack Web Development Course from
+                Programming Hero.
+              </p>
+            </div>
+            <hr />
+          </li>
+        </ul>
+      </Container>
+    </section>
+  );
 };
 
 export default ExperienceEducation;
